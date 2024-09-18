cdb6042cb93aace07f5e32e3349a3a67a13efb60
LeonardoPerandre <163353744+LeonardoPerandre@users.noreply.github.com>
2024-09-18 18:22:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativas = document.querySelector('.caixa-alternativas');
+const caixaResultado = document.querySelector('.caixa-resultado');
+
+const perguntas = [ //serve para abrir lista de perguntas
+    {//abre o objeto das perguntas
+        enunciado: "Pergunta 1",
+        alternativas: ["Alternativa 1","Alternativa 2"]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: ["Alternativa 1","Alternativa 2"]
+    },
+    {
+        enunciado: "Pergunta 3",
+        alternativas: ["Alternativa 1","Alternativa 2"]
+    }
+]
+
+let posicao = 0;
+let perguntaAtual;
+
+function mostraPergunta(){
+    perguntaAtual = perguntas[posicao];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas){
+        const botaoalternativas = document.createElement("button");
+        botaoalternativas.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoalternativas)
+    }
+}
+
+mostraPergunta();
