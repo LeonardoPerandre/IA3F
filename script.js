6fd6cbb76cbef2fcf2459a9466ce6e076f5f9b43
LeonardoPerandre <163353744+LeonardoPerandre@users.noreply.github.com>
2024-09-19 16:48:09 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,17 +5,30 @@ const caixaResultado = document.querySelector('.caixa-resultado');
 
 const perguntas = [ //serve para abrir lista de perguntas
     {//abre o objeto das perguntas
-        enunciado: "Pergunta 1",
+        enunciado: "A IA faz parte da sua vida?",
         alternativas: ["Alternativa 1","Alternativa 2"]
     },
     {
-        enunciado: "Pergunta 2",
+        enunciado: "è possível que a IA se torne ainda mais dominante no nosso dia a dia?",
         alternativas: ["Alternativa 1","Alternativa 2"]
     },
     {
-        enunciado: "Pergunta 3",
+        enunciado: "A IA está se tornando mais presente na vidas das pessoas?",
         alternativas: ["Alternativa 1","Alternativa 2"]
-    }
+    },
+      
+{
+    enunciado: "Você utiliza da IA em tarefas diárias?",
+    alternativas: ["Alternativa 1","Alternativa 2"]
+},
+    {
+    enunciado: "Você é usuário ativo das IA's??",
+    alternativas: ["Alternativa 1","Alternativa 2"]
+    },
+{
+    enunciado: "A IA é perigosa?",
+    alternativas: ["Alternativa 1","Alternativa 2"]
+}
 ]
 
 let posicao = 0;
