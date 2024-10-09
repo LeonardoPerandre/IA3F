0e957057914cbc7004befd6e8d9705783840277b
LeonardoPerandre <163353744+LeonardoPerandre@users.noreply.github.com>
2024-10-09 18:23:37 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -8,63 +8,63 @@ const perguntas = [ //serve para abrir lista de perguntas
         enunciado: "A IA faz parte da sua vida?",
         alternativas: 
             [{texto: "sim",
-            afirmação: "afirmação da alternativa"},   
+            afirmação: "A IA faz parte da minha vida"},   
             
             {texto:"não",
-             afirmação:"afirmação da alternativa"}
+             afirmação:"A IA não faz parte da minha vida"}
             ]
     },
     {
         enunciado: "è possível que a IA se torne ainda mais dominante no nosso dia a dia?",
         alternativas: [{texto:"sim",
-        afirmação: "afirmação da alternativa"}
+        afirmação: "É possível"}
         ]
         
         [{texto:"não",
-        afirmação:"afirmação da alternativa"}
+        afirmação:"Não é possível"}
         ]
     },
     {
         enunciado: "A IA está se tornando mais presente na vidas das pessoas?",
         alternativas: [{texto:"sim",
-        afirmação: "afirmação da alternativa"}
+        afirmação: "Está mais presente na vida das pessoas"}
         ]
         
         [{texto:"não",
-        afirmação:"afirmação da alternativa"}
+        afirmação:"A IA não esta mais presente na vida das pessoas"}
         ] 
     },
       
 {
     enunciado: "Você utiliza da IA em tarefas diárias?",
     alternativas: [{texto:"sim",
-    afirmação: "afirmação da alternativa"}
+    afirmação: "Utilizo"}
     ]
     
     [{texto:"não",
-    afirmação:"afirmação da alternativa"}
+    afirmação:"Não utilizo"}
     ]
 },
     {
     enunciado: "Você é usuário ativo das IA's??",
     alternativas: [{texto:"sim",
-    afirmação: "afirmação da alternativa"}
+    afirmação: "Utilizo"}
     ]
     
     [{texto:"não",
-    afirmação:"afirmação da alternativa"}
+    afirmação:"Não utilizo"}
     ]
     },
 {
     enunciado: "A IA é perigosa?",
     alternativas: [{texto:"sim",
-    afirmação: "afirmação da alternativa"}
+    afirmação: "É perigosa"}
     ]
     
     [{texto:"não",
-    afirmação:"afirmação da alternativa"}
+    afirmação:"Não é perigosa"}
     ]
-}
+},
 ]
 
 let posicao = 0;
@@ -72,8 +72,13 @@ let perguntaAtual;
 let historiafinal= "";
 
 function mostraPergunta(){
+    if(posicao>= perguntas.length){
+        mostraresultado();
+        return;
+    }
     perguntaAtual = perguntas[posicao];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent= " ";
     mostraAlternativas();
 }
 function mostraAlternativas(){
@@ -90,6 +95,10 @@ function respostaselecionada(opcaoselecionada){
     posicao++
     mostraPergunta();
 }
-
+function mostraresultado(){
+    caixaPerguntas.textContent = "opala 71...";
+    textoresultado.textContent = historiafinal;
+    caixaAlternativas.textContent = ""
+}
 mostraPergunta();
 
